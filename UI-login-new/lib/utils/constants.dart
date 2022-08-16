95790efcda681b1f729dfa378c673727877420b5
NNJio <99648810+NNJio@users.noreply.github.com>
2022-08-17 01:00:17 +0700

    Add files via upload

---
+++ UI-login-new/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color.fromARGB(255, 240, 172, 172);
+const kPrimaryColor = Color.fromARGB(255, 211, 69, 92);
+const kSecondaryColor = Color.fromARGB(255, 236, 226, 134);
